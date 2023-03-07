15ce5c5c5eeffdf9f2e14e14d9cb78639c9134ca
Matt Teeter <matt328@gmail.com>
2023-03-06 19:30:47 -0500

    ✨ feat(Pipelines): Add glslang helper to compile shaders to spirv at runtime

---
+++ Triton/SpirvHelper.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "glslang/SPIRV/GlslangToSpv.h"
+#include <vulkan/vulkan.hpp>
+
+class SpirvHelper {
+ public:
+   SpirvHelper();
+   ~SpirvHelper();
+
+   std::vector<uint8_t> compileShader(vk::ShaderStageFlagBits shaderType,
+                                      const char* shaderCode) const;
+
+ private:
+   static TBuiltInResource initResources();
+
+   static EShLanguage findLanguage(vk::ShaderStageFlagBits shaderType);
+};
