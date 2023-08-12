0c81feab5ca52e78609a77cdf86c13947fea621b
Matt <matt328@users.noreply.github.com>
2023-08-12 12:19:20 -0400

    feat(everything): 🔊 Adds tracy log sink

--- triton/include/Log.hpp
+++ triton/include/Log.hpp
@@ -2,6 +2,7 @@
 
 #include <memory>
 #include <spdlog/spdlog.h>
+#include "spdlog/sinks/base_sink.h"
 
 namespace Core {
    class Log {
@@ -9,5 +10,16 @@ namespace Core {
       static void init();
       static std::shared_ptr<spdlog::logger> core;
       static std::shared_ptr<spdlog::logger> game;
+   };
+
+   template <typename Mutex>
+   class TracySink : public spdlog::sinks::base_sink<Mutex> {
+    protected:
+      void sink_it_(const spdlog::details::log_msg& msg) override {
+         TracyMessage(msg.payload.data(), msg.payload.size());
+      }
+
+      void flush_() override {
+      }
    };
 }
