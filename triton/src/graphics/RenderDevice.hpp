cb586a9b58f5596c2f2bceb4ff488ffde6685ba5
Matt Teeter <matt328@gmail.com>
2023-08-14 15:09:55 -0400

    add nolintnextline for debug thing

--- triton/src/graphics/RenderDevice.hpp
+++ triton/src/graphics/RenderDevice.hpp
@@ -145,6 +145,7 @@ class RenderDevice {
                              const vk::raii::Device& device,
                              const vk::DebugReportObjectTypeEXT objectType,
                              const std::string_view name) {
+      // NOLINTNEXTLINE this is just debug anyway
       const auto debugHandle = reinterpret_cast<uint64_t>(static_cast<typename T::CType>(handle));
 
       const auto debugNameInfo = vk::DebugMarkerObjectNameInfoEXT{
