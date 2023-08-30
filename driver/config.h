7fcc22909f7bcaadb4915600525f4a70c12804fb
Matt <matt328@users.noreply.github.com>
2023-08-29 23:35:26 -0400

    Created new logger. Deleted old one. Everything is broken.

---
+++ driver/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define PROJECT_NAME "triton"
+#define PROJECT_VER "0.0.1"
+#define PROJECT_VER_MAJOR "0"
+#define PROJECT_VER_MINOR "0"
+#define PROJECT_VER_PATCH "1"
+
+#ifndef NDEBUG
+#define _DEBUG
+#endif
