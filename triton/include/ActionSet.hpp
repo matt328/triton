94b2bbd823bf6ad2a855a01c505cf95b10a97cdf
Matt Teeter <matt328@gmail.com>
2023-11-28 20:35:28 -0500

    refactor(layers): Refactoring scenes -> layers

--- triton/include/ActionSet.hpp
+++ triton/include/ActionSet.hpp
@@ -1,13 +1,7 @@
 #pragma once
 
-#include "Delegates.hpp"
-#include "Events.hpp"
 #include "Key.hpp"
 #include "ActionType.hpp"
-
-// Create ActionLayer class, have the actionSet still do most of what it does
-// but have action layers be separate sets of actions that can mapKey then add/Rmoeve
-// ActionListeners
 
 namespace Triton::Actions {
    class ActionSet {
