b682ec45971b6b56b21187f007fb4e20ef84aa9b
Matt Teeter <matt328@gmail.com>
2024-02-02 20:17:48 -0500

    Renames main target to triton

--- testarea/ScriptFactory.cxx
+++
@@ -1,9 +0,0 @@
-#include "ScriptFactory.hpp"
-
-ScriptFactory::ScriptFactory() {
-   lua.open_libraries(sol::lib::base);
-}
-
-std::unique_ptr<FooScript> ScriptFactory::createFooScript(std::string filename) {
-   lua.script_file(filename);
-}
