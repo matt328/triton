f4994dad4b0ed8729beb37d537c8cd1a5cbd9864
Matt Teeter <matt328@gmail.com>
2024-02-08 20:14:23 -0500

    sorting out action system some more

---
+++ src/actions/Mouse.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Triton::Actions {
+   enum class MouseInput {
+      MOVE_X,
+      MOVE_Y,
+      CLICK_LEFT,
+      CLICK_RIGHT,
+      CLICK_MIDDLE,
+      SCROLL_X,
+      SCROLL_Y
+   };
+}
