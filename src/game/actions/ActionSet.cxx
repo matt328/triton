b546c8ae93c8e3201678cabd8503ecbada69f4f7
Matt Teeter <matt328@gmail.com>
2024-02-13 10:45:51 -0500

    fixes mouse input

--- src/game/actions/ActionSet.cxx
+++ src/game/actions/ActionSet.cxx
@@ -3,9 +3,10 @@
 namespace Triton::Actions {
 
    void ActionSet::mapBool(Source source, ActionType actionType) {
-      actionTypeMap.insert({actionType, source});
+      boolMap.insert({actionType, source});
    }
 
    void ActionSet::mapFloat(Source source, ActionType actionType) {
+      floatMap.insert({actionType, source});
    }
 }
