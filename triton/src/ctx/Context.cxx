82af948e0d7c83f4b0f625e4169c44094870b09b
Matt Teeter <matt328@gmail.com>
2024-02-27 21:26:20 -0500

    docs(context): 📝 Documents why use empty destructors instead of = default'ing them.

--- triton/src/ctx/Context.cxx
+++ triton/src/ctx/Context.cxx
@@ -22,6 +22,9 @@ namespace tr::ctx {
       gameplayFacade = std::make_unique<GameplayFacade>(*gameplaySystem, *renderer);
    }
 
+   // Since a default destructor doesn't have access to the complete definition of the classes
+   // wrapped in smart pointers, it cannot call the destructors of forward declared classes.
+   // In short, this has to be here so all the member smart pointers will be able to be cleaned up.
    Context::~Context() {
    }
 
