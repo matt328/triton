22446f76302558c7493e955eb223c46f1c433e6c
Matt Teeter <matt328@gmail.com>
2024-03-04 20:58:34 -0500

    Working on file saving

--- editor/src/ProjectFile.cxx
+++ editor/src/ProjectFile.cxx
@@ -5,29 +5,54 @@
 
 namespace ed::io {
    using nlohmann::json;
-   void writeProjectFile(std::string_view filename) {
+   void writeProjectFile(std::string_view filename, tr::ctx::GameplayFacade& facade) {
       json j;
 
       j["name"] = "project name";
       j["activeCamera"] = "Camera #1";
 
-      json entity1, entity2;
-      entity1["components"] = {{{"type", "transform"},
-                                {"position", {{"x", 0.0}, {"y", 0.0}, {"z", 0.0}}},
-                                {"rotation", {{"x", 0.0}, {"y", 0.0}, {"z", 0.0}}}},
-                               {{"type", "editor-info"}, {"name", "some-entity"}},
-                               {{"type", "renderable-info"},
-                                {"sourceMesh", "relative filename"},
-                                {"sourceTexture", "relative filename"}}};
+      const auto& entities = facade.getAllEntities();
 
-      entity2["components"] = {{{"type", "editor-info"}, {"name", "entity name"}},
-                               {{"type", "transform"},
-                                {"position", {{"x", 0.0}, {"y", 0.0}, {"z", 0.0}}},
-                                {"rotation", {{"x", 0.0}, {"y", 0.0}, {"z", 0.0}}}},
-                               {{"type", "camera"}, {"yaw", -90.0}, {"pitch", 0.0}, {"fov", 60.0}}};
+      for (const auto& entity : entities) {
+         json je{};
+         const auto transform = facade.getEntityTransform(entity);
+         const auto entityInfo = facade.getEditorInfo(entity);
+         je["components"] = {{{"type", "transform"},
+                              {"position",
+                               {{"x", transform.position.x},
+                                {"y", transform.position.y},
+                                {"z", transform.position.z}}},
+                              {"rotation",
+                               {{"x", transform.rotation.x},
+                                {"y", transform.rotation.y},
+                                {"z", transform.rotation.z}}}}};
 
-      j["entities"] = {entity1, entity2};
-      Log::info << j.dump(4) << std::endl;
+         json editorInfo{};
+         editorInfo["type"] = "editor-info";
+         editorInfo["name"] = entityInfo.name;
+
+         if (entityInfo.sourceMesh.has_value()) {
+            editorInfo["sourceMesh"] = entityInfo.sourceMesh.value();
+         }
+
+         if (entityInfo.sourceTexture.has_value()) {
+            editorInfo["sourceTexture"] = entityInfo.sourceTexture.value();
+         }
+
+         je["components"].push_back(editorInfo);
+
+         const auto cameraComponent = facade.getCameraComponent(entity);
+         if (cameraComponent.has_value()) {
+            json cameraInfo{};
+            cameraInfo["type"] = "camera";
+            cameraInfo["yaw"] = cameraComponent.value().get().yaw;
+            cameraInfo["pitch"] = cameraComponent.value().get().pitch;
+            cameraInfo["fov"] = cameraComponent.value().get().fov;
+         }
+
+         j["entities"].push_back(je);
+      }
+      Log::info << j.dump(2) << std::endl;
    }
 
    void readProjectFile(entt::registry& registry, std::string_view& filename) {
