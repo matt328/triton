c8040bd1b23659b5c1f9a3f426eddf28d4aad608
Matt Teeter <matt328@gmail.com>
2024-03-06 23:45:05 -0500

    feat(gui): ✨ Cleans up saving/loading.
    Makes saving/loading behavior more predictable, adds unsaved indicator.

--- editor/src/Application.cxx
+++ editor/src/Application.cxx
@@ -1,14 +1,11 @@
 #include "Application.hpp"
+#include "Properties.hpp"
+
 #include "ctx/Context.hpp"
 #include "ctx/GameplayFacade.hpp"
 #include "util/Paths.hpp"
-#include "ImGuiStyle.hpp"
-#include "ImFileBrowser.hpp"
-#include <GLFW/glfw3.h>
-#include <imgui.h>
-#include <imgui_internal.h>
-#include "ProjectFile.hpp"
-#include "gp/ecs/component/Transform.hpp"
+
+#include "ui/Manager.hpp"
 
 #ifdef _WIN32
 #define GLFW_EXPOSE_NATIVE_WIN32
@@ -32,6 +29,10 @@ namespace ed {
 
    Application::Application(const int width, const int height, const std::string_view& windowTitle)
        : window(nullptr), context(nullptr), running(true) {
+
+      const auto configDir = std::filesystem::path(sago::getConfigHome()) / "editor";
+      pr::Properties::getInstance().load(configDir / "config.json");
+
       glfwInit();
       glfwSetErrorCallback(errorCallback);
       glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
@@ -105,20 +106,16 @@ namespace ed {
       context =
           std::make_unique<tr::ctx::Context>(window.get(), ImguiEnabled, GameplayDebugEnabled);
 
-      ImGuiEx::setupImGuiStyle();
+      manager = std::make_unique<ui::Manager>(context->getGameplayFacade());
 
-      // Editor should provide a way to load these things from a file and call this api
+      manager->addQuitListener<&tr::ctx::Context::hostWindowClosed>(context.get());
+      manager->setFullscreenFn([this]() { toggleFullscreen(*this); });
+
       auto& facade = context->getGameplayFacade();
 
       auto camera =
           facade.createCamera(width, height, Fov, ZNear, ZFar, CamStart, "Default Camera");
       facade.setCurrentCamera(camera);
-
-      ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-
-      // (optional) set browser properties
-      fileDialog.SetTitle("Load Project");
-      fileDialog.SetTypeFilters({".json"});
    }
 
    Application::~Application() {
@@ -133,157 +130,10 @@ namespace ed {
          ImGui::NewFrame();
          ImGui::ShowDemoWindow();
 
-         auto& facade = context->getGameplayFacade();
-
-         renderDockSpace();
-
-         renderEntityEditor(facade);
+         manager->render();
 
          ImGui::Render();
       });
-   }
-
-   void Application::renderDockSpace() {
-      static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_PassthruCentralNode;
-      ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
-
-      ImGuiViewport* viewport = ImGui::GetMainViewport();
-      ImGui::SetNextWindowPos(viewport->Pos);
-      ImGui::SetNextWindowSize(viewport->Size);
-      ImGui::SetNextWindowViewport(viewport->ID);
-      ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-      ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-      window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse |
-                      ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-      window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-
-      if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
-         window_flags |= ImGuiWindowFlags_NoBackground;
-
-      ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-      ImGui::Begin("DockSpace", nullptr, window_flags);
-      ImGui::PopStyleVar();
-      ImGui::PopStyleVar(2);
-
-      // DockSpace
-      ImGuiIO& io = ImGui::GetIO();
-      if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable) {
-         ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
-         ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
-
-         static auto first_time = true;
-         if (first_time) {
-            first_time = false;
-
-            ImGui::DockBuilderRemoveNode(dockspace_id); // clear any previous layout
-            ImGui::DockBuilderAddNode(dockspace_id, dockspace_flags | ImGuiDockNodeFlags_DockSpace);
-            ImGui::DockBuilderSetNodeSize(dockspace_id, viewport->Size);
-            auto dock_id_left = ImGui::DockBuilderSplitNode(dockspace_id,
-                                                            ImGuiDir_Left,
-                                                            0.2f,
-                                                            nullptr,
-                                                            &dockspace_id);
-            ImGui::DockBuilderDockWindow("Entity Editor", dock_id_left);
-            ImGui::DockBuilderFinish(dockspace_id);
-         }
-      }
-      ImGui::End(); // Dockspace
-
-      if (ImGui::BeginMainMenuBar()) {
-         if (ImGui::BeginMenu("File")) {
-            if (ImGui::MenuItem("Open", "Ctrl+O")) {
-               context->getGameplayFacade().clear();
-               io::readProjectFile(std::string_view{"some_file.json"},
-                                   context->getGameplayFacade());
-            }
-            if (ImGui::MenuItem("Save", "Ctrl+S")) {
-               io::writeProjectFile(std::string_view{"some_file.json"},
-                                    context->getGameplayFacade());
-            }
-            if (ImGui::MenuItem("Exit", "Alt+F4")) {
-               context->hostWindowClosed();
-            }
-            ImGui::EndMenu();
-         }
-         if (ImGui::BeginMenu("Edit")) {
-            if (ImGui::MenuItem("Undo", "CTRL+Z")) {}
-            if (ImGui::MenuItem("Redo", "CTRL+Y", false, false)) {} // Disabled item
-            ImGui::Separator();
-            if (ImGui::MenuItem("Cut", "CTRL+X")) {}
-            if (ImGui::MenuItem("Copy", "CTRL+C")) {}
-            if (ImGui::MenuItem("Paste", "CTRL+V")) {}
-            ImGui::EndMenu();
-         }
-         ImGui::EndMainMenuBar();
-      }
-   }
-
-   void Application::renderEntityEditor(tr::ctx::GameplayFacade& facade) {
-      auto& es = facade.getAllEntities();
-
-      if (ImGui::Begin("Entity Editor")) {
-         // Left
-         ImGui::BeginChild("left pane",
-                           ImVec2(150, 0),
-                           ImGuiChildFlags_Border | ImGuiChildFlags_ResizeX);
-         for (auto e : es) {
-            auto& infoComponent = facade.getEditorInfo(e);
-            if (ImGui::Selectable(infoComponent.name.c_str(),
-                                  static_cast<uint32_t>(e) == selectedEntity)) {
-               selectedEntity = static_cast<uint32_t>(e);
-            }
-         }
-         ImGui::EndChild();
-         ImGui::SameLine();
-
-         // Right
-         {
-            ImGui::BeginGroup();
-            ImGui::BeginChild(
-                "item view",
-                ImVec2(0,
-                       -ImGui::GetFrameHeightWithSpacing())); // Leave room for 1 line below us
-
-            if (selectedEntity.has_value()) {
-               ImGui::Text("Entity ID: %d", selectedEntity.value());
-            } else {
-               ImGui::Text("No Entity Selected");
-            }
-
-            if (selectedEntity.has_value()) {
-               const auto maybeTransform = facade.getComponent<tr::gp::ecs::Transform>(
-                   static_cast<entt::entity>(selectedEntity.value()));
-               if (maybeTransform.has_value()) {
-                  auto& transform = maybeTransform.value().get();
-                  ImGui::SeparatorText("Transform");
-                  ImGui::DragFloat3("Position", glm::value_ptr(transform.position), .1f);
-                  ImGui::DragFloat3("Rotation",
-                                    glm::value_ptr(transform.rotation),
-                                    .1f,
-                                    -180.f,
-                                    180.f);
-                  ImGui::SeparatorText("Renderable");
-               }
-            }
-            ImGui::EndChild();
-
-            if (ImGui::Button("New...")) {
-               fileDialog.Open();
-            }
-            ImGui::SameLine();
-            if (ImGui::Button("Save")) {}
-
-            ImGui::EndGroup();
-         }
-      }
-      ImGui::End();
-
-      fileDialog.Display();
-
-      if (fileDialog.HasSelected()) {
-         Log::info << "Selected filename " << fileDialog.GetSelected().string() << std::endl;
-         fileDialog.ClearSelected();
-      }
    }
 
    // GLFW Callbacks
@@ -309,33 +159,38 @@ namespace ed {
       if (ImGui::GetIO().WantCaptureKeyboard || ImGui::GetIO().WantTextInput) {
          return;
       }
-      const auto app = static_cast<Application*>(glfwGetWindowUserPointer(window));
+      auto app = static_cast<Application*>(glfwGetWindowUserPointer(window));
       if (key == GLFW_KEY_ENTER && mods == GLFW_MOD_ALT && action == GLFW_RELEASE) {
-         if (app->fullscreen) {
-            glfwSetWindowMonitor(window,
-                                 nullptr,
-                                 app->prevXPos,
-                                 app->prevYPos,
-                                 app->prevWidth,
-                                 app->prevHeight,
-                                 0);
-            app->fullscreen = !app->fullscreen;
-         } else {
-            const auto currentMonitor = glfwGetPrimaryMonitor();
-            const auto mode = glfwGetVideoMode(currentMonitor);
-            glfwGetWindowPos(window, &app->prevXPos, &app->prevYPos);
-            glfwGetWindowSize(window, &app->prevWidth, &app->prevHeight);
-            glfwSetWindowMonitor(window,
-                                 currentMonitor,
-                                 0,
-                                 0,
-                                 mode->width,
-                                 mode->height,
-                                 mode->refreshRate);
-            app->fullscreen = !app->fullscreen;
-         }
+         toggleFullscreen(*app);
       } else {
          app->context->keyCallback(key, scancode, action, mods);
+      }
+   }
+
+   void Application::toggleFullscreen(Application& app) {
+      auto window = app.window.get();
+      if (app.fullscreen) {
+         glfwSetWindowMonitor(window,
+                              nullptr,
+                              app.prevXPos,
+                              app.prevYPos,
+                              app.prevWidth,
+                              app.prevHeight,
+                              0);
+         app.fullscreen = !app.fullscreen;
+      } else {
+         const auto currentMonitor = glfwGetPrimaryMonitor();
+         const auto mode = glfwGetVideoMode(currentMonitor);
+         glfwGetWindowPos(window, &app.prevXPos, &app.prevYPos);
+         glfwGetWindowSize(window, &app.prevWidth, &app.prevHeight);
+         glfwSetWindowMonitor(window,
+                              currentMonitor,
+                              0,
+                              0,
+                              mode->width,
+                              mode->height,
+                              mode->refreshRate);
+         app.fullscreen = !app.fullscreen;
       }
    }
 
