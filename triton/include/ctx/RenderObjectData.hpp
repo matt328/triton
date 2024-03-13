872884b1219fb6c0f3fdec5bacea0f3ac5cf90a2
Matt Teeter <matt328@gmail.com>
2024-03-13 08:18:09 -0400

    continuing ResourceLoader

--- triton/include/ctx/RenderObjectData.hpp
+++ triton/include/ctx/RenderObjectData.hpp
@@ -1,5 +1,95 @@
 #pragma once
 
+#include "gfx/geometry/Vertex.hpp"
+
 namespace tr::ctx {
-   struct RenderObjectData {};
+
+   using Vertex = gfx::Geometry::Vertex;
+
+   struct Primitive {
+      std::vector<uint32_t> indices;
+      std::vector<Vertex> vertices;
+   };
+
+   struct Material {};
+
+   class RenderObjectDataIterator {
+    public:
+      using iterator_category = std::forward_iterator_tag;
+      using value_type = std::pair<Primitive&, Material&>;
+      using difference_type = std::ptrdiff_t;
+      using pointer = value_type*;
+      using reference = value_type&;
+
+      RenderObjectDataIterator(std::vector<Primitive>& primitives,
+                               std::vector<Material>& materials,
+                               std::unordered_map<size_t, size_t>& primitivesToMaterials,
+                               size_t index)
+          : primitives(primitives),
+            materials(materials),
+            primitivesToMaterials(primitivesToMaterials),
+            index(index) {
+      }
+
+      RenderObjectDataIterator& operator++() {
+         ++index;
+         return *this;
+      }
+
+      RenderObjectDataIterator operator++(int) {
+         auto temp = *this;
+         ++(*this);
+         return temp;
+      }
+
+      bool operator==(const RenderObjectDataIterator& other) const {
+         return index == other.index;
+      }
+
+      bool operator!=(const RenderObjectDataIterator& other) const {
+         return !(*this == other);
+      }
+
+      value_type operator*() const {
+         size_t primitiveIndex = index;
+         size_t materialIndex = primitivesToMaterials.at(primitiveIndex);
+         return {primitives[primitiveIndex], materials[materialIndex]};
+      }
+
+    private:
+      std::vector<Primitive>& primitives;
+      std::vector<Material>& materials;
+      std::unordered_map<size_t, size_t>& primitivesToMaterials;
+      size_t index;
+   };
+
+   class RenderObjectData {
+    public:
+      using Iterator = RenderObjectDataIterator;
+      RenderObjectData();
+      ~RenderObjectData();
+
+      RenderObjectData(const RenderObjectData&) = delete;
+      RenderObjectData(RenderObjectData&&) = delete;
+      RenderObjectData& operator=(const RenderObjectData&) = delete;
+      RenderObjectData& operator=(RenderObjectData&&) = delete;
+
+      void addPrimitive(Primitive&& p, Material&& m);
+
+      Iterator begin() {
+         return RenderObjectDataIterator{primitives, materials, primitivesToMaterials, 0};
+      }
+
+      Iterator end() {
+         return RenderObjectDataIterator{primitives,
+                                         materials,
+                                         primitivesToMaterials,
+                                         primitives.size()};
+      }
+
+    private:
+      std::vector<Primitive> primitives;
+      std::vector<Material> materials;
+      std::unordered_map<size_t, size_t> primitivesToMaterials;
+   };
 }
--- triton/src/ctx/ResourceLoader.cxx
+++ triton/src/ctx/ResourceLoader.cxx
@@ -2,10 +2,14 @@
 
 #include "ctx/RenderObjectData.hpp"
 #include "gfx/Handles.hpp"
+#include "gfx/geometry/Vertex.hpp"
 
 #include <fastgltf/core.hpp>
+#include <fastgltf/util.hpp>
 
 namespace tr::ctx {
+
+   using Vertex = gfx::Geometry::Vertex;
 
    ResourceLoader::ResourceLoader(gfx::Renderer& renderer) : renderer(renderer) {
    }
@@ -27,7 +31,23 @@ namespace tr::ctx {
       data.loadFromFile(path);
 
       auto asset = parser.loadGltf(&data, path.parent_path());
-      if (auto error = asset.error(); error != fastgltf::Error::None) {}
+      if (auto error = asset.error(); error != fastgltf::Error::None) {
+         std::stringstream ss{};
+         ss << "Failed to load gltf file: " << path.string() << fastgltf::to_underlying(error)
+            << std::endl;
+         throw std::runtime_error(ss.str());
+      }
+
+      std::vector<RenderObjectData> renderObjectData;
+
+      for (auto& mesh : asset->meshes) {
+         for (auto&& p : mesh.primitives) {
+            {
+               auto& indexAccessor = asset->accessors[p.indicesAccessor.value()];
+               indices.reserve(indices.size() +);
+            }
+         }
+      }
 
       return gfx::RenderObjectHandle{3};
    }
