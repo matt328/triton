0b5c1c9d38b6eef1d0934ba5a358c5703641673d
Matt Teeter <matt328@gmail.com>
2024-04-30 08:06:24 -0400

    starts animation system in ecs

--- triton/src/gp/ecs/system/RenderDataSystem.cxx
+++ triton/src/gp/ecs/system/RenderDataSystem.cxx
@@ -2,6 +2,7 @@
 
 #include "gfx/ObjectData.hpp"
 #include "gfx/RenderData.hpp"
+#include "gp/ecs/component/Animation.hpp"
 #include "gp/ecs/component/Camera.hpp"
 #include "gp/ecs/component/DebugConstants.hpp"
 #include "gp/ecs/component/Renderable.hpp"
@@ -33,6 +34,22 @@ namespace tr::gp::ecs::RenderDataSystem {
             }
             renderData.objectData.emplace_back(transform.transformation, it.second);
          }
+      }
+
+      const auto animationsView = registry.view<Animation>();
+      for (auto [error, entity, animationData] : animationsView.each()) {
+         /*
+            - Add joint matrices to an array in RenderData.
+            - Also need to add a dynamicMeshData that includes a MeshHandle, TextureHandle, and a
+            JoinMatrixHandle
+            - JointMatrixHandle will need an offset into jointMatrices array and maybe also a size?
+            - When rendering, this JointMatrixHandle will need to be part of ObjectData
+            - extend ObjectData to have a uint jointMatrixOffset, in the shader, use that to look up
+            the model's first joint matrix from the buffer
+            - Still need to understand if that's all that needs done is multiplying each vertex's
+            transformation by the weighted joint matrix in the shader, and if the animation library
+            takes care of everything else
+         */
       }
 
       const auto debugView = registry.view<Transform, DebugConstants>();
