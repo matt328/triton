428c35a2b50188a2aece9abb8eb3b60daab8d675
Matt Teeter <matt328@gmail.com>
2024-05-11 12:52:05 -0400

    Adds comments

--- triton/src/gfx/sb/DSShaderBinding.hpp
+++ triton/src/gfx/sb/DSShaderBinding.hpp
@@ -16,13 +16,18 @@ namespace tr::gfx::sb {
 
       void bindImageSamplers(const uint32_t binding,
                              const std::vector<vk::DescriptorImageInfo>& imageInfo) override;
-
+      /// Binds a ShaderBinding to a spot in the pipeline layout.
+      /// setIndex refers to the spot in the pipeline layout this descriptor is bound to.
+      /// The descriptor's layout must match the layout found at that index in the pipeline layout.
       void bindToPipeline(const vk::raii::CommandBuffer& cmd,
                           const vk::PipelineBindPoint bindPoint,
-                          const uint32_t setNumber,
+                          const uint32_t setIndex,
                           const vk::PipelineLayout& layout) const override;
 
     private:
+      // Note to Self: A reference is the appropriate choice here since it conveys the fact that the
+      // Device is intended to outlive the DSShaderBinding. The linter doesn't like it, but in this
+      // game architecture it is the right choice.
       const vk::raii::Device& device;
       std::unique_ptr<vk::raii::DescriptorSet> vkDescriptorSet;
       vk::DescriptorType descriptorType;
