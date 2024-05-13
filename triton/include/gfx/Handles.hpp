a8faeb7db151c0d3cf4198c495e136377282da23
Matt Teeter <matt328@gmail.com>
2024-05-13 08:42:57 -0400

    continues animation

--- triton/include/gfx/Handles.hpp
+++ triton/include/gfx/Handles.hpp
@@ -5,6 +5,15 @@ namespace tr::gfx {
    using MeshHandle = size_t;
    using MaterialHandle = uint32_t;
    using ModelInfoHandle = uint32_t;
+   using JointMatricesHandle = uint32_t;
    using MeshMaterialHandle = std::unordered_map<MaterialHandle, std::vector<MeshHandle>>;
+
+   /// unordered_map<MeshHandle, TextureHandle>
    using ModelHandle = std::unordered_map<MeshHandle, TextureHandle>;
+
+   struct SkinnedModelHandle {
+      MeshHandle meshHandle;
+      TextureHandle textureHandle;
+      JointMatricesHandle jointMatrixHandle;
+   };
 }
