a5b18cb2a21056814bc850483bfce60bb4f455e5
Matt Teeter <matt328@gmail.com>
2024-08-05 10:30:58 -0400

    style(everything): Continue code cleanup.

---
+++ apps/testarea/src/Data.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+struct Book {
+   explicit Book(const std::string& name) : name(name) {
+   }
+   std::string name;
+};
+
+struct Shelf {
+   explicit Shelf(const Book& newBook) : book(newBook) {
+   }
+   Book book;
+};
+
+struct Library {
+   explicit Library(const Shelf& newShelf) : shelf(newShelf) {
+   }
+   Shelf shelf;
+};
