757f5461e340e342559a6af96127153341ec1eb4
Matt Teeter <matt328@gmail.com>
2024-08-12 11:02:23 -0400

    build(everything): Disable compiler warnings on fetched content.
    I don't need to see all that.

--- cmake/compiler-options.cmake
+++ cmake/compiler-options.cmake
@@ -14,41 +14,41 @@
 # the License.
 
 option(ENABLE_ALL_WARNINGS "Compile with all warnings for the major compilers"
-       OFF)
-option(ENABLE_EFFECTIVE_CXX "Enable Effective C++ warnings" OFF)
+  ON)
+option(ENABLE_EFFECTIVE_CXX "Enable Effective C++ warnings" ON)
 option(GENERATE_DEPENDENCY_DATA "Generates .d files with header dependencies"
-       OFF)
+  OFF)
 
-if(ENABLE_ALL_WARNINGS)
-  if(CMAKE_C_COMPILER_ID MATCHES "GNU"
-     OR CMAKE_CXX_COMPILER_ID MATCHES "GNU"
-     OR CMAKE_C_COMPILER_ID MATCHES "(Apple)?[Cc]lang"
-     OR CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?[Cc]lang")
+if (ENABLE_ALL_WARNINGS)
+  if (CMAKE_C_COMPILER_ID MATCHES "GNU"
+    OR CMAKE_CXX_COMPILER_ID MATCHES "GNU"
+    OR CMAKE_C_COMPILER_ID MATCHES "(Apple)?[Cc]lang"
+    OR CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?[Cc]lang")
     # GCC/Clang
     add_compile_options(-Wall -Wextra)
-  elseif(MSVC)
+  elseif (MSVC)
     # MSVC
     add_compile_options(/W4)
-  endif()
-endif()
+  endif ()
+endif ()
 
-if(ENABLE_EFFECTIVE_CXX)
-  if(CMAKE_C_COMPILER_ID MATCHES "GNU"
-     OR CMAKE_CXX_COMPILER_ID MATCHES "GNU"
-     OR CMAKE_C_COMPILER_ID MATCHES "(Apple)?[Cc]lang"
-     OR CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?[Cc]lang")
+if (ENABLE_EFFECTIVE_CXX)
+  if (CMAKE_C_COMPILER_ID MATCHES "GNU"
+    OR CMAKE_CXX_COMPILER_ID MATCHES "GNU"
+    OR CMAKE_C_COMPILER_ID MATCHES "(Apple)?[Cc]lang"
+    OR CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?[Cc]lang")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Weffc++")
-  endif()
-endif()
+  endif ()
+endif ()
 
-if(GENERATE_DEPENDENCY_DATA)
-  if(CMAKE_C_COMPILER_ID MATCHES "GNU"
-     OR CMAKE_CXX_COMPILER_ID MATCHES "GNU"
-     OR CMAKE_C_COMPILER_ID MATCHES "(Apple)?[Cc]lang"
-     OR CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?[Cc]lang")
+if (GENERATE_DEPENDENCY_DATA)
+  if (CMAKE_C_COMPILER_ID MATCHES "GNU"
+    OR CMAKE_CXX_COMPILER_ID MATCHES "GNU"
+    OR CMAKE_C_COMPILER_ID MATCHES "(Apple)?[Cc]lang"
+    OR CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?[Cc]lang")
     add_compile_options(-MD)
-  else()
+  else ()
     message(
       WARNING "Cannot generate header dependency on non GCC/Clang compilers.")
-  endif()
-endif()
+  endif ()
+endif ()
