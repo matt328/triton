3bc6ca2410ec219e4fe9cafc60569e4e19d4ad43
Matt Teeter <matt328@gmail.com>
2024-08-25 18:22:55 -0400

    Change editor properties to use cereal for serialization instead of json

--- libs/triton/graphics/src/Paths.hpp
+++ libs/triton/graphics/src/Paths.hpp
@@ -3,5 +3,4 @@
 namespace tr::util::Paths {
    const std::filesystem::path RESOURCES = std::filesystem::current_path() / "assets";
    const std::filesystem::path SHADERS = RESOURCES / "shaders";
-   const std::filesystem::path TEXTURES = RESOURCES / "textures";
 }
