7e9c1cd19c3fc697ec7730c3d476bf7d0b9d0370
Matt Teeter <matt328@gmail.com>
2024-09-22 21:19:31 -0400

    Remove warning-riddled noiseutils

--- libs/triton/graphics/src/HeightField.cxx
+++ libs/triton/graphics/src/HeightField.cxx
@@ -1,34 +1,19 @@
 #include "HeightField.hpp"
 
-#include <noise/noise.h>
-#include "NoiseUtils.hpp"
+#include <sdf/FastNoiseLite.h>
 
 namespace tr::ct {
 
-   using namespace noise;
-   constexpr double LowerX = 2.0;
-   constexpr double UpperX = 6.0;
-   constexpr double LowerZ = 1.0;
-   constexpr double UpperZ = 5.0;
-   constexpr float MaxHeight = 20;
-
    HeightField::HeightField(const int size) : width{size} {
 
-      const module::Perlin myModule;
-      utils::NoiseMap heightMap;
-      utils::NoiseMapBuilderPlane heightMapBuilder;
-      heightMapBuilder.SetSourceModule(myModule);
-      heightMapBuilder.SetDestNoiseMap(heightMap);
-      heightMapBuilder.SetDestSize(size, size);
-      heightMapBuilder.SetBounds(LowerX, UpperX, LowerZ, UpperZ);
-      heightMapBuilder.Build();
+      auto fn = FastNoiseLite();
 
       for (int x = 0; x < size; x++) {
          for (int y = 0; y < size; y++) {
-            auto value = heightMap.GetValue(x, y);
+            auto value = fn.GetNoise(static_cast<float>(x), static_cast<float>(y));
 
             value = value + 1.0f;
-            value = value * MaxHeight / 2;
+            value = value * 512.f / 2;
             data.push_back(value);
          }
       }
