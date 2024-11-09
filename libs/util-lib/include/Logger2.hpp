545628c0786995d3cf4ba0c24b0d0eac4ec5d422
Matt Teeter <matt328@gmail.com>
2024-11-09 10:11:41 -0500

    Clean up main

--- libs/util-lib/include/Logger2.hpp
+++ libs/util-lib/include/Logger2.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
+#include <spdlog/common.h>
 #include <spdlog/spdlog.h>
 
 extern spdlog::logger Log;
 
-void initLogger();
+void initLogger(spdlog::level::level_enum debugLevel, spdlog::level::level_enum releaseLevel);
