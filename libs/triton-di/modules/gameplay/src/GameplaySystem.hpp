cd0d008162e94411c1f478bd3a92d33e2ebe8618
Matt Teeter <matt328@gmail.com>
2024-11-20 08:08:30 -0500

    Start EntityEditor

--- libs/triton-di/modules/gameplay/src/GameplaySystem.hpp
+++ libs/triton-di/modules/gameplay/src/GameplaySystem.hpp
@@ -1,17 +1,21 @@
 #pragma once
 
+#include "tr/IEventBus.hpp"
 #include "tr/IGameplaySystem.hpp"
 #include "gp/action/IActionSystem.hpp"
+#include "CommandQueue.hpp"
+#include <entt/entity/fwd.hpp>
 
 namespace tr::gp {
    class GameplaySystem : public IGameplaySystem {
     public:
-      explicit GameplaySystem(const std::shared_ptr<IActionSystem>& actionSystem);
+      explicit GameplaySystem(const std::shared_ptr<IActionSystem>& actionSystem,
+                              std::shared_ptr<IEventBus> newEventBus);
       ~GameplaySystem() override = default;
 
-      GameplaySystem(const GameplaySystem&) = default;
+      GameplaySystem(const GameplaySystem&) = delete;
       GameplaySystem(GameplaySystem&&) = delete;
-      auto operator=(const GameplaySystem&) -> GameplaySystem& = default;
+      auto operator=(const GameplaySystem&) -> GameplaySystem& = delete;
       auto operator=(GameplaySystem&&) -> GameplaySystem& = delete;
 
       void update() override;
@@ -24,7 +28,15 @@ namespace tr::gp {
       auto createTerrain() -> cm::EntityType override;
       auto createDefaultCamera() -> cm::EntityType override;
 
+      [[nodiscard]] auto getRegistry() const -> entt::registry& override;
+      [[nodiscard]] auto getConstRegistry() const -> const entt::registry& override;
+
     private:
+      std::shared_ptr<IEventBus> eventBus;
+
       RenderDataTransferHandler transferHandler;
+      std::shared_mutex registryMutex{};
+      std::unique_ptr<entt::registry> registry;
+      std::unique_ptr<CommandQueue<entt::registry>> commandQueue;
    };
 }
