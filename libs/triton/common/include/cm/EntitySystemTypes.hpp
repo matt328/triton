be85b0e367e2a5ad47437fde17ff57202acbd9bb
Matt Teeter <matt328@gmail.com>
2024-11-22 10:33:36 -0500

    Remove old versions of files

--- libs/triton/common/include/cm/EntitySystemTypes.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <entt/fwd.hpp>
-
-namespace tr::cm {
-   using EntityType = entt::entity;
-   using TerrainCreatedFn = std::function<void(cm::EntityType)>;
-} // namespace tr::cm
