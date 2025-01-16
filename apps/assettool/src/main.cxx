77057ca81da81a1d257882dc4cf334c8591b0f66
Matt Teeter <matt328@gmail.com>
2025-01-16 12:21:53 -0500

    Refactor for readability.

--- apps/assettool/src/main.cxx
+++ apps/assettool/src/main.cxx
@@ -32,77 +32,95 @@ auto parseCommandLine(const std::vector<std::string>& args) {
   return options;
 }
 
+auto parseConverterOptions(const std::vector<std::string>& args) {
+  namespace fs = std::filesystem;
+  auto options = parseCommandLine(args);
+  const auto gltfFileStr = options["f"];
+
+  auto gltfFile = fs::path{gltfFileStr};
+
+  if (gltfFile.is_relative()) {
+    gltfFile = absolute(gltfFile);
+  }
+
+  std::optional<fs::path> skeletonFile;
+  if (options.contains("s")) {
+    skeletonFile = std::make_optional(fs::path{options["s"]});
+    if (skeletonFile.value().is_relative()) {
+      skeletonFile = std::make_optional(absolute(skeletonFile.value()));
+    }
+  }
+
+  auto outputFile = fs::path{options["o"]};
+  if (outputFile.is_relative()) {
+    outputFile = absolute(outputFile);
+  }
+
+  return as::ModelResources{.modelPath = gltfFile,
+                            .skeletonPath = skeletonFile,
+                            .outputFile = outputFile};
+}
+
+auto writeOutputFile(const std::filesystem::path& outputFile, const as::Model& tritonModel)
+    -> bool {
+  if (outputFile.extension() == ".json") {
+    std::ofstream outputStream(outputFile);
+    if (!outputStream) {
+      return false;
+    }
+
+    cereal::JSONOutputArchive jsonOutput(outputStream);
+    jsonOutput(tritonModel);
+    Log.info("Wrote json output file to {0}", outputFile.string());
+    return true;
+  }
+
+  if (outputFile.extension() == ".trm") {
+    std::ofstream outputStream(outputFile, std::ios::binary);
+    if (!outputStream) {
+      return false;
+    }
+
+    cereal::PortableBinaryOutputArchive binOutput(outputStream);
+    binOutput(tritonModel);
+    outputStream.close();
+    Log.info("Wrote binary output file to {0}", outputFile.string());
+    return true;
+  }
+
+  Log.error("Unsupported file extension: {0}", outputFile.extension().string());
+  return false;
+}
+
 auto main(int argc, char* argv[]) -> int {
-  std::vector<std::string> args(argv, argv + argc);
   initLogger(spdlog::level::trace, spdlog::level::trace);
 
-  namespace fs = std::filesystem;
+  std::vector<std::string> args(argv, argv + argc);
+
+  auto converterOptions = parseConverterOptions(args);
+
   try {
-    if (auto options = parseCommandLine(args); options["mode"] == "gltf") {
-      const auto gltfFileStr = options["f"];
-      auto gltfFile = fs::path{gltfFileStr};
 
-      if (gltfFile.is_relative()) {
-        gltfFile = absolute(gltfFile);
-      }
+    const auto injector =
+        di::make_injector(di::bind<as::ITransformParser>.to<as::GltfTransformParser>(),
+                          di::bind<as::IGeometryExtractor>.to<as::GltfGeometryExtractor>(),
+                          di::bind<as::ITextureExtractor>.to<as::GltfTextureExtractor>(),
+                          di::bind<as::ISkinningDataExtractor>.to<as::GltfSkinningDataExtractor>(),
+                          di::bind<as::IModelLoader<tinygltf::Model>>.to<as::GltfModelLoader>(),
+                          di::bind<as::ISkeletonLoader>.to<as::GltfSkeletonLoader>());
 
-      std::optional<fs::path> skeletonFile;
-      if (options.contains("s")) {
-        skeletonFile = std::make_optional(fs::path{options["s"]});
-        if (skeletonFile.value().is_relative()) {
-          skeletonFile = std::make_optional(absolute(skeletonFile.value()));
-        }
-      }
+    auto modelConverter = injector.create<std::shared_ptr<as::ModelConverter>>();
 
-      auto outputFile = fs::path{options["o"]};
-      if (outputFile.is_relative()) {
-        outputFile = absolute(outputFile);
-      }
+    modelConverter->load(converterOptions);
+    auto tritonModel = modelConverter->buildTritonModel();
 
-      Log.info("Converting Gltf file, input: {0}, skeleton file: {1}",
-               gltfFile.string(),
-               skeletonFile.has_value() ? skeletonFile.value().string() : "none provided");
+    Log.info("Writing file with serialization version {0}", as::SERIAL_VERSION);
 
-      const auto injector = di::make_injector(
-          di::bind<as::ITransformParser>.to<as::GltfTransformParser>(),
-          di::bind<as::IGeometryExtractor>.to<as::GltfGeometryExtractor>(),
-          di::bind<as::ITextureExtractor>.to<as::GltfTextureExtractor>(),
-          di::bind<as::ISkinningDataExtractor>.to<as::GltfSkinningDataExtractor>(),
-          di::bind<as::IModelLoader<tinygltf::Model>>.to<as::GltfModelLoader>(),
-          di::bind<as::ISkeletonLoader>.to<as::GltfSkeletonLoader>());
-
-      auto modelConverter = injector.create<std::shared_ptr<as::ModelConverter>>();
-
-      {
-        modelConverter->load({.modelPath = gltfFile, .skeletonPath = skeletonFile});
-        auto tritonModel = modelConverter->buildTritonModel();
-
-        Log.info("Writing file with serialization version {0}", as::SERIAL_VERSION);
-
-        if (outputFile.extension() == ".json") {
-          if (std::ofstream outputStream(outputFile); outputStream) {
-            cereal::JSONOutputArchive jsonOutput(outputStream);
-            jsonOutput(tritonModel);
-            Log.info("Wrote json output file to {0}", outputFile.string());
-          } else {
-            Log.error("Failed to open output file {0}", outputFile.string());
-          }
-        } else if (outputFile.extension() == ".trm") {
-          if (std::ofstream outputStream(outputFile, std::ios::binary); outputStream) {
-            cereal::PortableBinaryOutputArchive binOutput(outputStream);
-            binOutput(tritonModel);
-            outputStream.close();
-            Log.info("Wrote binary output file to {0}", outputFile.string());
-          } else {
-            Log.error("Failed to open output file {0}", outputFile.string());
-          }
-        }
-      }
-
-    } else {
-      Log.error("First arg must be 'gltf' for now");
+    if (!writeOutputFile(converterOptions.outputFile, tritonModel)) {
+      Log.error("Failed to open output file {0}", converterOptions.outputFile.string());
       return -1;
     }
+
   } catch (const std::exception& ex) {
     Log.error(ex.what());
     return -1;
