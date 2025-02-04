93f00fcbc1a590d42e471b59c3b23604f6095dd8
Matt Teeter <matt328@gmail.com>
2025-02-04 09:02:53 -0500

    Optimizing MeshBufferManager

--- libs/triton/modules/graphics/src/vk/MeshBufferManager.cxx
+++ libs/triton/modules/graphics/src/vk/MeshBufferManager.cxx
@@ -34,18 +34,38 @@ auto MeshBufferManager::addMesh(const IGeometryData& geometryData) -> MeshHandle
   auto ibInsertPosition = ibMaxAllocatedOffset;
   {
     // Find an empty block and set insert position there if one is found
-    const auto emptyBlock = findEmptyBlock(geometryData, emptyIndexBlocks);
+    auto emptyBlock = findEmptyBlock(geometryData.getIndexCount(), emptyIndexBlocks);
     if (emptyBlock.has_value()) {
-      ibInsertPosition = emptyBlock->offset;
+      ibInsertPosition = emptyBlock.value().get().offset;
+      // Either Shrink or remove the empty block
+      auto newEmptyBlockSize = emptyBlock.value().get().size - geometryData.getIndexCount();
+      if (newEmptyBlockSize == 0) {
+        auto it = std::find(emptyIndexBlocks.begin(), emptyIndexBlocks.end(), *emptyBlock);
+        emptyIndexBlocks.erase(it);
+      } else {
+        auto newEmptyBlockOffset = emptyBlock.value().get().offset + geometryData.getIndexCount();
+        emptyBlock.value().get().offset = newEmptyBlockOffset;
+        emptyBlock.value().get().size = newEmptyBlockSize;
+      }
     }
   }
 
   // Same as Index Buffer
   auto vbInsertPosition = vbMaxAllocatedOffset;
   {
-    const auto emptyBlock = findEmptyBlock(geometryData, emptyVertexBlocks);
+    auto emptyBlock = findEmptyBlock(geometryData.getVertexCount(), emptyVertexBlocks);
     if (emptyBlock.has_value()) {
-      vbInsertPosition = emptyBlock->offset;
+      vbInsertPosition = emptyBlock.value().get().offset;
+      // Either Shrink or remove the empty block
+      auto newEmptyBlockSize = emptyBlock.value().get().size - geometryData.getVertexCount();
+      if (newEmptyBlockSize == 0) {
+        auto it = std::find(emptyVertexBlocks.begin(), emptyVertexBlocks.end(), *emptyBlock);
+        emptyVertexBlocks.erase(it);
+      } else {
+        auto newEmptyBlockOffset = emptyBlock.value().get().offset + geometryData.getVertexCount();
+        emptyBlock.value().get().offset = newEmptyBlockOffset;
+        emptyBlock.value().get().size = newEmptyBlockSize;
+      }
     }
   }
 
@@ -53,16 +73,24 @@ auto MeshBufferManager::addMesh(const IGeometryData& geometryData) -> MeshHandle
   const auto ibInsertPositionInBytes = ibStride * ibInsertPosition;
   auto ibNeedsResize = ibInsertPositionInBytes + geometryData.getIndexDataSize() > ibMaxSize;
   if (ibInsertPosition == ibMaxAllocatedOffset && ibNeedsResize) {
-    vbHandle = bufferManager->resizeBuffer(vbHandle, vbMaxSize * 2);
-    vbMaxSize *= 2;
+    auto newSize = ibMaxSize;
+    while (newSize < ibMaxSize + geometryData.getIndexDataSize()) {
+      newSize *= 2;
+    }
+    ibHandle = bufferManager->resizeBuffer(ibHandle, newSize);
+    ibMaxSize = newSize;
   }
 
   // Same as Index buffer
   const auto vbInsertPositionInBytes = vbStride * vbInsertPosition;
   auto vbNeedsResize = vbInsertPositionInBytes + geometryData.getVertexDataSize() > vbMaxSize;
   if (vbInsertPosition == vbMaxAllocatedOffset && vbNeedsResize) {
-    ibHandle = bufferManager->resizeBuffer(ibHandle, ibMaxSize * 2);
-    ibMaxSize *= 2;
+    auto newSize = vbMaxSize;
+    while (newSize < vbMaxSize + geometryData.getVertexDataSize()) {
+      newSize *= 2;
+    }
+    vbHandle = bufferManager->resizeBuffer(vbHandle, newSize);
+    vbMaxSize = newSize;
   }
 
   // Add the data to the buffers at the determined offsets
@@ -101,10 +129,19 @@ auto MeshBufferManager::getIndexBufferHandle() const -> BufferHandle {
 
 auto MeshBufferManager::removeMesh(MeshHandle meshHandle) -> void {
   auto bufferEntry = bufferEntries[meshHandle];
-  emptyIndexBlocks.emplace_back(
-      Block{.offset = bufferEntry.indexOffset, .size = bufferEntry.indexCount});
-  emptyVertexBlocks.emplace_back(
-      Block{.offset = bufferEntry.vertexOffset, .size = bufferEntry.vertexCount});
+
+  auto [it, inserted] =
+      emptyIndexBlocks.insert(Block{bufferEntry.indexOffset, bufferEntry.indexCount});
+  if (inserted) {
+    mergeWithNeighbors(it, emptyIndexBlocks);
+  }
+
+  auto [itV, insertedV] =
+      emptyVertexBlocks.insert(Block{bufferEntry.vertexOffset, bufferEntry.vertexCount});
+  if (insertedV) {
+    mergeWithNeighbors(itV, emptyVertexBlocks);
+  }
+
   bufferEntries.erase(meshHandle);
 }
 
@@ -117,13 +154,15 @@ auto MeshBufferManager::getGpuBufferEntries(const std::vector<RenderMeshData>& m
 
   for (const auto& meshData : meshDataList) {
     ZoneNamedN(var, "processMeshData", true);
-    const auto& bufferEntry = bufferEntries[meshData.handle];
-    gpuBufferEntryList.push_back(GpuBufferEntry{.indexCount = bufferEntry.indexCount,
-                                                .firstIndex = bufferEntry.indexOffset,
-                                                .vertexOffset = bufferEntry.vertexOffset,
-                                                .instanceCount = 1,
-                                                .firstInstance = 0,
-                                                .padding = 0});
+    if (bufferEntries.contains(meshData.handle)) {
+      const auto& bufferEntry = bufferEntries.at(meshData.handle);
+      gpuBufferEntryList.push_back(GpuBufferEntry{.indexCount = bufferEntry.indexCount,
+                                                  .firstIndex = bufferEntry.indexOffset,
+                                                  .vertexOffset = bufferEntry.vertexOffset,
+                                                  .instanceCount = 1,
+                                                  .firstInstance = 0,
+                                                  .padding = 0});
+    }
   }
 
   return gpuBufferEntryList;
@@ -134,14 +173,68 @@ auto MeshBufferManager::getBuffers() const -> std::tuple<Buffer&, Buffer&> {
           bufferManager->getBuffer(getIndexBufferHandle())};
 }
 
-auto MeshBufferManager::findEmptyBlock(const IGeometryData& geometryData,
-                                       const std::vector<Block>& blocks) -> std::optional<Block> {
-  for (const auto& block : blocks) {
-    if (geometryData.getIndexDataSize() <= block.size) {
-      return std::make_optional(block);
+auto MeshBufferManager::mergeWithNeighbors(BlockContainer::iterator it, BlockContainer& blocks)
+    -> void {
+  if (it == blocks.end()) {
+    return;
+  }
+
+  auto prev = (it == blocks.begin()) ? blocks.end() : std::prev(it);
+  auto next = std::next(it);
+
+  bool merged = false;
+  Block mergedBlock = *it;
+
+  if (prev != blocks.end() && prev->offset + prev->size == it->offset) {
+    mergedBlock.offset = prev->offset;
+    mergedBlock.size += prev->size;
+    blocks.erase(prev);
+    merged = true;
+  }
+
+  if (next != blocks.end() && it->offset + it->size == next->offset) {
+    mergedBlock.size += next->size;
+    blocks.erase(next);
+    merged = true;
+  }
+
+  if (merged) {
+    blocks.erase(it);
+    blocks.insert(mergedBlock);
+  }
+}
+
+auto MeshBufferManager::findEmptyBlock(uint32_t requiredSize, BlockContainer& blocks)
+    -> std::optional<std::reference_wrapper<Block>> {
+
+  auto it = blocks.lower_bound(Block{.offset = 0, .size = requiredSize});
+  if (it == blocks.end()) {
+    return std::nullopt;
+  }
+  return std::ref(const_cast<Block&>(*it));
+}
+
+auto MeshBufferManager::mergeFreeBlocks(BlockContainer& blocks) -> void {
+
+  if (blocks.empty()) {
+    return;
+  }
+
+  auto it = blocks.begin();
+  while (it != blocks.end()) {
+    auto next = std::next(it);
+    if (next != blocks.end() && it->offset + it->size == next->offset) {
+      // Merge blocks
+      Block mergedBlock{.offset = it->offset, .size = it->size + next->size};
+
+      // Erase both old blocks and insert merged block
+      it = blocks.erase(it);
+      it = blocks.erase(it);
+      it = blocks.insert(it, mergedBlock);
+    } else {
+      ++it;
     }
   }
-  return std::nullopt;
 }
 
 }
