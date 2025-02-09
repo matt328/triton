60460f8706b3ae9c9680b88922d72258c2a732f7
Matt Teeter <matt328@gmail.com>
2025-02-09 11:03:24 -0500

    Update FindOzz.cmake to find appropriate debug vs release libs

--- cmake/FindOzz.cmake
+++ cmake/FindOzz.cmake
@@ -4,15 +4,22 @@ find_path(OZZ_INCLUDE_DIR
   DOC "Path to the Ozz Animation headers"
 )
 
-# Try to find the Ozz library files
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+  set(CMAKE_FIND_LIBRARY_SUFFIXES "_d.so" "_d.a" "_d.lib")
+endif()
+
+if(CMAKE_BUILD_TYPE STREQUAL "Release")
+  set(CMAKE_FIND_LIBRARY_SUFFIXES "_r.so" "_r.a" "_r.lib")
+endif()
+
 find_library(OZZ_ANIMATION_LIBRARY
-  NAMES ozz_animation_d
+  NAMES ozz_animation
   PATHS ${CMAKE_SOURCE_DIR}/vendor/ozz/lib
   DOC "Path to the Ozz Animation library"
 )
 
 find_library(OZZ_BASE_LIBRARY
-  NAMES ozz_base_d
+  NAMES ozz_base
   PATHS ${CMAKE_SOURCE_DIR}/vendor/ozz/lib
   DOC "Path to the Ozz Base library"
 )
