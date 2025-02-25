880fa14e8ff91852f754e15a2418c44864b52c73
Matt Teeter <matt328@gmail.com>
2025-02-25 08:57:38 -0500

    Start designing editor data store to support synchronized EntityService

--- apps/cauldron/src/data/DataFacade.hpp
+++ apps/cauldron/src/data/DataFacade.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#include "GlmCereal.hpp"
 #include "cm/EntitySystemTypes.hpp"
 #include "cm/TaskQueue.hpp"
 #include "gp/components/Transform.hpp"
 #include "tr/IGameplaySystem.hpp"
+#include "DataStore.hpp"
 
 /*
    Think of the editor as a tool for editing the project data nothing more.
@@ -26,80 +26,6 @@ class IGameplaySystem;
 }
 
 namespace ed {
-
-struct SkeletonData {
-  std::string name;
-  std::string filePath;
-
-  template <class T>
-  void serialize(T& archive) {
-    archive(name, filePath);
-  }
-};
-
-struct AnimationData {
-  std::string name;
-  std::string filePath;
-
-  template <class T>
-  void serialize(T& archive) {
-    archive(name, filePath);
-  }
-};
-
-struct ModelData {
-  std::string name;
-  std::string filePath;
-
-  template <class T>
-  void serialize(T& archive) {
-    archive(name, filePath);
-  }
-};
-
-struct EntityData {
-  std::string name;
-  glm::vec3 position{glm::zero<glm::vec3>()};
-  glm::vec3 rotation{glm::zero<glm::vec3>()};
-
-  std::string modelName;
-  std::string skeleton;
-  std::vector<std::string> animations;
-
-  glm::vec3 camPosition{glm::zero<glm::vec3>()};
-  float yaw{0.f};
-  float pitch{0.f};
-
-  template <class T>
-  void serialize(T& archive) {
-    archive(name, position, rotation, modelName, skeleton, animations, camPosition, yaw, pitch);
-  }
-};
-
-struct TerrainData {
-  std::string name;
-  // adjustable terrain params
-  // Right now this is just a marker for the engine to do render whatever it thinks a terrain is
-  template <class T>
-  void serialize(T& archive) {
-    archive(name);
-  }
-};
-
-struct DataStore {
-  // Assets
-  std::unordered_map<std::string, SkeletonData> skeletons;
-  std::unordered_map<std::string, AnimationData> animations;
-  std::unordered_map<std::string, ModelData> models;
-
-  // Scene
-  std::unordered_map<std::string, EntityData> scene;
-
-  template <class T>
-  void serialize(T& archive) {
-    archive(skeletons, animations, models, scene);
-  }
-};
 
 class DataFacade {
 public:
@@ -156,15 +82,15 @@ public:
     return engineBusy;
   }
 
-  [[nodiscard]] const auto& getSkeletons() const {
+  [[nodiscard]] auto getSkeletons() const -> const auto& {
     return dataStore.skeletons;
   }
 
-  [[nodiscard]] const auto& getAnimations() const {
+  [[nodiscard]] auto getAnimations() const -> const auto& {
     return dataStore.animations;
   }
 
-  [[nodiscard]] const auto& getModels() const {
+  [[nodiscard]] auto getModels() const -> const auto& {
     return dataStore.models;
   }
 
