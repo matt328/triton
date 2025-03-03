f8d26eb1d8c7a64105c5cbd9af9f037946a981db
Matt Teeter <matt328@gmail.com>
2025-03-03 15:36:02 -0500

    Add entity service create terrain

--- libs/triton/modules/graphics/src/TerrainManager.cxx
+++ libs/triton/modules/graphics/src/TerrainManager.cxx
@@ -8,4 +8,11 @@ TerrainManager::TerrainManager() {
 TerrainManager::~TerrainManager() {
 }
 
+auto TerrainManager::registerTerrain(std::string_view name, glm::vec3 size) -> void {
+}
+
+auto TerrainManager::getChunks(std::string_view name) -> std::vector<ChunkDefinition>& {
+  return {};
+}
+
 }
