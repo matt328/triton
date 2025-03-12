d6a6f05abcc47806316467d0c0a2da56393ddf38
Matt Teeter <matt328@gmail.com>
2025-03-12 08:06:09 -0400

    Box kinda works. suspect vertex winding order is reversed.

--- libs/triton/modules/triton/src/SdfGenerator.cxx
+++ libs/triton/modules/triton/src/SdfGenerator.cxx
@@ -1,14 +1,22 @@
 #include "tr/SdfGenerator.hpp"
 #include "tr/PlaneGenerator.hpp"
+#include "tr/BoxGenerator.hpp"
 
 namespace tr {
 
 auto SdfGenerator::registerGenerator(SdfCreateInfo createInfo) -> GeneratorHandle {
   const auto key = keyGen.getKey();
+
   if (createInfo.shapeType == ShapeType::Plane) {
     const auto info = createInfo.get<PlaneInfo>();
     generatorMap.emplace(key, std::make_shared<PlaneGenerator>(info.normal, info.height));
   }
+
+  if (createInfo.shapeType == ShapeType::Box) {
+    const auto info = createInfo.get<BoxInfo>();
+    generatorMap.emplace(key, std::make_shared<BoxGenerator>(info.center, info.size));
+  }
+
   return key;
 }
 
