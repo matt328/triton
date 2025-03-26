2259eda9db48ba2b5e1901e121720b0f7db5e48c
Matt Teeter <matt328@gmail.com>
2025-03-25 21:12:54 -0400

    Static and Dynamic Model loading and rendering functioning again.

--- libs/graphics-vk/src/ResourceProxyImpl.cxx
+++ libs/graphics-vk/src/ResourceProxyImpl.cxx
@@ -1,16 +1,43 @@
 #include "ResourceProxyImpl.hpp"
 
 #include "VkResourceManager.hpp"
+#include "geo/DynamicGeometryData.hpp"
+#include "geo/StaticGeometryData.hpp"
 
 namespace tr {
 
+/// This ProxyImpl keeps vk-graphics from knowing about as::Model
 ResourceProxyImpl::ResourceProxyImpl(std::shared_ptr<VkResourceManager> newResourceManager)
     : resourceManager{std::move(newResourceManager)} {
 }
 
-auto ResourceProxyImpl::uploadModel([[maybe_unused]] const as::Model& model) -> ModelData {
-  // return resourceManager->uploadModel(model);
-  return ModelData{};
+auto ResourceProxyImpl::uploadModel(const as::Model& model) -> ModelData {
+  // Convert a Model into IGeometryData
+
+  MeshHandle meshHandle = 0L;
+
+  if (model.skinned()) {
+    auto verts = model.dynamicVertices.value();
+    auto indices = model.indices;
+    auto geometryData = DynamicGeometryData{std::move(verts), std::move(indices)};
+    meshHandle = resourceManager->uploadDynamicMesh(geometryData);
+  } else {
+    auto verts = model.staticVertices.value();
+    auto indices = model.indices;
+    auto geometryData = StaticGeometryData{std::move(verts), std::move(indices)};
+    meshHandle = resourceManager->uploadStaticMesh(geometryData);
+  }
+
+  auto textureHandle = resourceManager->uploadImage(model.imageData, "unnamed_texture");
+  auto meshData = MeshData{.meshHandle = meshHandle,
+                           .topology = Topology::Triangles,
+                           .textureHandle = textureHandle};
+
+  auto skinData = SkinData{
+      .jointMap = model.jointRemaps,
+      .inverseBindMatrices = model.inverseBindPoses,
+  };
+  return ModelData{.meshData = meshData, .skinData = skinData, .animationData = std::nullopt};
 }
 
 }
